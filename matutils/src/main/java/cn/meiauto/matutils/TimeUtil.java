4ae8137866ed4532f64664d85ea822ff2f4ae973
LiYang <yang.li@meiauto.cn>
2018-02-26 15:27:42 +0800

    modified time util

--- matutils/src/main/java/cn/meiauto/matutils/TimeUtil.java
+++ matutils/src/main/java/cn/meiauto/matutils/TimeUtil.java
@@ -1,13 +1,27 @@
 package cn.meiauto.matutils;
 
+import android.text.TextUtils;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
+@SuppressWarnings("ALL")
 public class TimeUtil {
+
+    public static String getDate(long time) {
+        return new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date(time));
+    }
 
     public static String getDate() {
         return new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+    }
+
+    public static String getDateAndTime(String filter) {
+        return new SimpleDateFormat(filter, Locale.getDefault()).format(new Date());
     }
 
     public static String getDateAndTime() {
@@ -22,7 +36,70 @@ public class TimeUtil {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date(time));
     }
 
-    public static String getDateAndTimeWithoutDivider() {
-        return new SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(new Date());
+    public static String getDateAndTime(long time, String filter) {
+        return new SimpleDateFormat(filter, Locale.getDefault()).format(new Date(time));
+    }
+
+    public static String getDateAndTimeNoSeconds(long time) {
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault()).format(new Date(time));
+    }
+
+    public static long getStartTimeOfDay(long now, String timeZone) {
+        String tz = TextUtils.isEmpty(timeZone) ? "GMT+8" : timeZone;
+        TimeZone curTimeZone = TimeZone.getTimeZone(tz);
+        Calendar calendar = Calendar.getInstance(curTimeZone);
+        calendar.setTimeInMillis(now);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTimeInMillis();
+    }
+
+    public static long getStartTimeOfNextDay(long now, String timeZone) {
+        String tz = TextUtils.isEmpty(timeZone) ? "GMT+8" : timeZone;
+        TimeZone curTimeZone = TimeZone.getTimeZone(tz);
+        Calendar calendar = Calendar.getInstance(curTimeZone);
+        calendar.setTimeInMillis(now);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.add(Calendar.DAY_OF_YEAR, 1);//加一天
+        return calendar.getTimeInMillis();
+    }
+
+    public static long parseTime(String time) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        try {
+            return dateFormat.parse(time).getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public static long parseTime(String pattern, String time) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(pattern, Locale.getDefault());
+        try {
+            return dateFormat.parse(time).getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    public static String getDuration(long duration) {
+        long nd = 1000 * 24 * 60 * 60;
+        long nh = 1000 * 60 * 60;
+        long nm = 1000 * 60;
+        long hour = duration % nd / nh;
+        long min = duration % nd % nh / nm;
+
+        if (hour == 0) {
+            return min + "分钟";
+        } else {
+            return hour + "小时" + min + "分钟";
+        }
     }
 }
